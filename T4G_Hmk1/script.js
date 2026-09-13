59ff01f7b149e8bef06357e2d62209c884384425
miraimport <queenmira7705@gmail.com>
2026-09-13 10:49:58 +0000

    compelet T4G Homework 1

---
+++ T4G_Hmk1/script.js
@@ -0,0 +1,39 @@
+// A. Store your name and log it
+let name = "Samira Ali";
+console.log('A. Name:', name);
+
+// B. Store your age and log it
+let age = 36;
+console.log('B. Age:', age);
+
+// C. Add two numbers
+let addition = 10 + 5;
+console.log('C. Addition:', addition);
+
+// D. Subtract two numbers
+let subtraction = 10 - 7;
+console.log('D. Subtraction:', subtraction);
+
+// E. Divide two numbers
+let division = 10 / 2;
+console.log('E. Division:', division);
+
+// F. Multiply two numbers
+let multiplication = 10 * 5;
+console.log('F. Multiplication:', multiplication);
+
+// G. Find the remainder of 15 % 4
+let remainder = 15 % 4;
+console.log('G. Remainder:', remainder);
+
+// H. Store a boolean value
+let isStudent = true;
+console.log('H. Boolean:', isStudent);
+
+// I. Store a number and log its data type
+let numberValue = 25;
+console.log('I. Type of numberValue:', typeof numberValue);
+
+// J. Store a string and log its data type
+let stringValue = "Hello";
+console.log('J. Type of stringValue:', typeof stringValue);
